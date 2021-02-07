681ddb1f1f4b79d84d596cb32f2a827bda468a50
SumaiyaNimi <66512131+SumaiyaNimi@users.noreply.github.com>
2021-02-07 04:25:57 -0600

    Add files via upload

---
+++ sample.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTutorials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = "Spring 2021 CS 5525 Cloud Computing\n";
+
+            Console.WriteLine(message);
+	    string message = "Assignment 1";
+
+            Console.WriteLine(message);
+	    string message = "My website using MS Azure\n";
+
+            Console.WriteLine(message);
+	    string message = "Sumaiya Tabassum Nimi, ID: 16302918";
+
+            Console.WriteLine(message);
+        }
+
+
+    }
+}
